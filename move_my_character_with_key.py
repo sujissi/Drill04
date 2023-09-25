b84174ad246f2ba15506c852a5f2c967bcf294cc
sujissi_Laptop <hwangsuji329@gmail.com>
2023-09-25 17:54:07 +0900

    이미지 로드

---
+++ move_my_character_with_key.py
@@ -0,0 +1,6 @@
+from pico2d import *
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+character = load_image('my_character_sheet.png')

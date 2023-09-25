5d2cf72469e4280beab50a3bbc905257ffbb870c
sujissi_Laptop <hwangsuji329@gmail.com>
2023-09-25 20:57:10 +0900

    방향키 구현

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -3,10 +3,16 @@ from pico2d import *
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
-character = load_image('my_character_sheet.png')
+character = load_image('cat_sheet.png')
 
 running = True
+x = TUK_WIDTH//2
+y = TUK_HEIGHT//2
+frame = 0
 dir_x, dir_y = 0, 0
+img_x, img_y = 0, 0
+
+hide_cursor()
 def handle_events():
     global running, dir_x, dir_y
     events = get_events()
@@ -33,5 +39,16 @@ def handle_events():
                 dir_y -= 1
             elif event.key == SDLK_DOWN:
                 dir_y += 1
+
+while running:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
+    character.clip_draw(frame*img_x, img_y, 35, 50, x, y, 100, 100)
+    update_canvas()
+    handle_events()
+    frame = (frame + 1) % 3
+    x += dir_x * 5
+    y += dir_y * 5
+    delay(0.05)
 
 close_canvas()

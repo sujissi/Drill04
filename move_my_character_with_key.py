03882ea8dce1579a9d11c3604844384d5c73ffad
sujissi_Laptop <hwangsuji329@gmail.com>
2023-09-25 18:16:52 +0900

    키 이벤트 함수 구현
    상하좌우 키 입력 따라 방향 값 설정

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -4,3 +4,34 @@ TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('my_character_sheet.png')
+
+running = True
+dir_x, dir_y = 0, 0
+def handle_events():
+    global running, dir_x, dir_y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir_x += 1
+            elif event.key == SDLK_LEFT:
+                dir_x -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_x -= 1
+            elif event.key == SDLK_LEFT:
+                dir_x += 1
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
+
+close_canvas()

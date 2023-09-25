cb238be5351b612336f601691e70cba370a22851
sujissi_Laptop <hwangsuji329@gmail.com>
2023-09-25 21:54:49 +0900

    애니매이션 구현 성공, 화면 경계 충돌체크 뼈대 만듬

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,6 +6,7 @@ tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('cat_sheet.png')
 
 running = True
+collision = False
 x = TUK_WIDTH//2
 y = TUK_HEIGHT//2
 frame = 0
@@ -14,7 +15,7 @@ dir_img = 0
 
 hide_cursor()
 def handle_events():
-    global running, dir_x, dir_y, dir_img, frame
+    global running, dir_x, dir_y, dir_img
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -44,7 +45,7 @@ def handle_events():
             elif event.key == SDLK_DOWN:
                 dir_y += 1
     if dir_x == 0 and dir_y == 0 :
-        frame, dir_img = 1, 3
+        dir_img = 3
 
 while running:
     clear_canvas()
@@ -53,8 +54,9 @@ while running:
     update_canvas()
     handle_events()
     frame = (frame + 1) % 3
-    x += dir_x * 10
-    y += dir_y * 10
+    if not is_collision(x,y):
+        x += dir_x * 10
+        y += dir_y * 10
     delay(0.05)
 
 close_canvas()
